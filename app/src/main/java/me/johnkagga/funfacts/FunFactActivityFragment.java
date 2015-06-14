dc9be3381a4a062e396f77cf68c827afdbd8f9cd
jokamjohn <johnkagga@gmail.com>
2015-06-14 10:52:56 +0300

    Changing color in the background

--- app/src/main/java/me/johnkagga/funfacts/FunFactActivityFragment.java
+++ app/src/main/java/me/johnkagga/funfacts/FunFactActivityFragment.java
@@ -1,11 +1,13 @@
 package me.johnkagga.funfacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -15,6 +17,7 @@ import android.widget.TextView;
 public class FunFactActivityFragment extends Fragment {
 
     FunBook mFunBook = new FunBook();
+
 
     public FunFactActivityFragment() {
     }
@@ -30,12 +33,14 @@ public class FunFactActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootVeiw = inflater.inflate(R.layout.fragment_main, container, false);
 
+        final RelativeLayout mLayout =(RelativeLayout)rootVeiw.findViewById(R.id.relative_layout);
         final TextView factText = (TextView)rootVeiw.findViewById(R.id.factTextView);
         Button factbutton = (Button)rootVeiw.findViewById(R.id.show_facts_button);
         View.OnClickListener factListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = mFunBook.getFact();
+                mLayout.setBackgroundColor(Color.RED);
                 factText.setText(fact);
 
             }
