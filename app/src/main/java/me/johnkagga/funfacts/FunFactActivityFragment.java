bc34fb6a47192994f9e67fae171347eab7b918ad
jokamjohn <johnkagga@gmail.com>
2015-06-14 03:21:19 +0300

    Added a button listener and new fact

--- app/src/main/java/me/johnkagga/funfacts/FunFactActivityFragment.java
+++ app/src/main/java/me/johnkagga/funfacts/FunFactActivityFragment.java
@@ -1,10 +1,12 @@
 package me.johnkagga.funfacts;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -16,8 +18,28 @@ public class FunFactActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootVeiw = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final TextView factText = (TextView)rootVeiw.findViewById(R.id.factTextView);
+        Button factbutton = (Button)rootVeiw.findViewById(R.id.show_facts_button);
+        View.OnClickListener factListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = "Ostriches run faster than horses";
+                factText.setText(fact);
+
+            }
+        };
+
+        factbutton.setOnClickListener(factListener);
+        return rootVeiw;
     }
 }
